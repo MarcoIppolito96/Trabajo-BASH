2c9e07effc471ebe4b8c3ac8ad9594bdf96ced6e
Marco Ippolito <marcoippolito96@gmail.com>
2023-06-28 16:56:32 -0300

    Actualizacion de generar.sh y agregado del archivo con nombres de personas

--- generar.sh
+++ generar.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
-if [[ #? != 1]]; then
-	echo "Numero de argumentos incorrecto, debe usar solo uno."
+if [[ $# != 1 ]]; then
+	echo "Numero de argumentos incorrecto, debe usar solo uno"
 	exit 1
 fi
 
-mkdir itpm
+mkdir itmp
 
+sort -R personas.csv | cut -d "," -f 1 | head -n $1 > nombres
 
-for $(seq $1); do
+#for $(seq $1); do
 
-wget -P itmp -O $1 URL_archivo
-sleep 5
+#wget -P itmp URL_archivo
+#sleep 5
 
+#done
 
+for file in imagen*; do
+	read line
+	mv "${file}" "${line}"
+done < nombres
 
-done
