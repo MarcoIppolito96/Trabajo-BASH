8fbc68a6da9ac2d6099f3d826491a0bb61cdf51c
Marco Ippolito <marcoippolito96@gmail.com>
2023-06-27 21:28:27 -0300

    Primeras lineas en generar

--- generar.sh
+++ generar.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+if [[ #? != 1]]; then
+	echo "Numero de argumentos incorrecto, debe usar solo uno."
+	exit 1
+fi
+
+mkdir itpm
+
+
+for $(seq $1); do
+
+wget -P itmp -O $1 URL_archivo
+sleep 5
+
+
+
+done
