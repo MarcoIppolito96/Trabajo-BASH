5fe4f25da23725596c5a019fa4f93d1577c1a0b2
romanvrd <82334858+romanvrd@users.noreply.github.com>
2023-07-05 22:58:07 -0300

    Actualización de generar.sh. Se quitó dependencia innecesaria del Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,14 @@ FROM ubuntu
 MAINTAINER Marco Ippolito, Román Vázquez Ruiz Díaz
 
 # Instalación de paquetes necesarios
-RUN apt-get update && apt-get install -y wget gzip imagemagick
+RUN apt-get update && apt-get install -y wget imagemagick
 RUN apt-get clean
 RUN rm -rf /var/lib/apt/lists/*
 
 # Se crea un directorio en el cual se montará el directorio raíz
 RUN mkdir /opt/procesador
-VOLUME /opt/procesador
 WORKDIR /opt/procesador
+
+# Se establece la ruta en donde se montará el directorio.
+VOLUME /opt/procesador
 CMD [ "bash", "menu.sh" ]
--- generar.sh
+++ generar.sh
@@ -3,16 +3,38 @@ if [[ $# != 1 ]]; then
 	exit 1
 fi
 
+# Constantes
+ARCHIVO_NOMBRES="personas.csv"
+DIR_TEMPORAL=".tmp"
+LINK=https://thispersondoesnotexist.com/
+FORMATO="jpg"
+ARCHIVO_SALIDA=imagenes.tar.gz
+
 # Se crea una lista de nombres a partir de el archivo CSV provisto.
-NOMBRES=$(sort -R personas.csv | head -n $1 | cut -d "," -f 1 | tr " " _)
+NOMBRES=$(sort -R $ARCHIVO_NOMBRES | head -n $1 | cut -d "," -f 1 | cut -d " " -f 1)
 
 # Se crea un directorio temporal para guardar las imágenes.
-mkdir tmp 2> /dev/null
+mkdir $DIR_TEMPORAL 2> /dev/null
 
+# Se descargan las imágenes, asignándoseles un nombre de la lista de nombres.
 CONT=0
-for nombre in $NOMBRES; do
-	wget -O tmp/$nombre.jpg https://thispersondoesnotexist.com/ 2> /dev/null
+for NOMBRE in $NOMBRES; do
+	wget -O "$DIR_TEMPORAL/$NOMBRE.$FORMATO" $LINK 2> /dev/null
 	CONT=$((CONT + 1))
 	echo "Generando imágenes... ($CONT/$1)"
 	sleep 1
 done
+
+echo "Imágenes generadas. Comprimiendo..."
+
+# Se comprimen las imágenes
+tar -C $DIR_TEMPORAL -czf $ARCHIVO_SALIDA ./
+
+# Se calcula la suma de verificación y se guarda en un archivo
+echo $(sha256sum $ARCHIVO_SALIDA) > $ARCHIVO_SALIDA.sha256
+
+# Se borra el directorio temporal
+rm -r .tmp
+
+echo "Imágenes generadas y comprimidas en $ARCHIVO_SALIDA"
+read -p "Presione cualquier tecla para continuar..."
